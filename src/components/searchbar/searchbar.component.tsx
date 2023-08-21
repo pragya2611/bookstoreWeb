2108b2a9026409d6dc16de43edec7023bc658f42
pragya2611 <pragya2611@gmail.com>
2023-08-21 19:25:12 +0530

    header details

--- src/components/searchbar/searchbar.component.tsx
+++ src/components/searchbar/searchbar.component.tsx
@@ -1,46 +1,47 @@
+import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+import InputBase from "@mui/material/InputBase";
+import { alpha, styled } from "@mui/material/styles";
+import * as React from "react";
+import { ApplicationConstants } from "../../core/constants";
+import searchbarStyles from './searchbar.module.scss';
 
-import SearchIcon from '@mui/icons-material/Search';
-import Box from '@mui/material/Box';
-import InputBase from '@mui/material/InputBase';
-import { alpha, styled } from '@mui/material/styles';
-import * as React from 'react';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
@@ -70,24 +71,18 @@ export default function PrimarySearchAppBar() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-
-
- 
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-         
-       
+    <Box sx={{ flexGrow: 1 }} className={searchbarStyles.searchbox}>
+      <Search className={searchbarStyles.searchBarWrapper}>
+        <StyledInputBase
+          className={searchbarStyles.searchInput}
+          placeholder= {ApplicationConstants.searchContent}
+          inputProps={{ "aria-label": ApplicationConstants.searchContent }}
+        />
+        <SearchIconWrapper className={searchbarStyles.searchIconWrapper}>
+          <SearchIcon />
+        </SearchIconWrapper>
+      </Search>
     </Box>
   );
 }
